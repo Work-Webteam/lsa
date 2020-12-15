c049a3942c102a2ed938061e57aae63bb6073ca4
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2020-12-14 16:05:36 -0800

    Updated Export Recipients report to enable refined styling and reduced row-per-page count, also removed cancel button.

--- templates/Registrations/exportrecipients.php
+++ templates/Registrations/exportrecipients.php
@@ -1,38 +1,40 @@
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css"/>
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css"/>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-
-<?= $this->Html->css('lsa-datatables.css') ?>
-
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<!-- JQuery TODO: Exterminate! -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+<!-- JZip -->
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+
+<!-- PDF Generation Scripts -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 
-<h2>Export Recipients - <?php echo date("Y"); ?></h2>
+<!-- Custom Miscellaneous Script-->
+<script type="text/javascript" src="/js/lsa.js"></script>
+
+
+<h2 class="page-title">Export Recipients - <?php echo date("Y"); ?></h2>
 
 
 <div class="datatable-container">
     <?= $this->Flash->render() ?>
-    <table id="data-table-1" class="display ceremony-datatable" style="font-size: 12px; width:100%">
+    <table id="data-table-1" class="table thead-dark table-striped table-sm" >
 
     </table>
 </div>
 
 
 <?php
-
+/*
 echo $this->Form->button('Cancel', array(
     'type' => 'button',
     'onclick' => 'location.href=\'/registrations/\'',
     'class' => 'btn btn-secondary',
 ));
-
+*/
 ?>
 
 
@@ -115,7 +117,8 @@ echo $this->Form->button('Cancel', array(
 
             ],
             // stateSave: true,
-            pageLength: 15,
+            pageLength: 12,
+            pagingType: 'simple',
             lengthChange: false,
             // order: [[ 1, "asc" ]],
 
