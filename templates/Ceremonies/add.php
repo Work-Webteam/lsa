e2f0ccd59f91f9cbf1828149a81371aafbadfe0d
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2021-02-01 10:33:16 -0800

    Updated add ceremonies form to incorporate bootstrap theme.

--- templates/Ceremonies/add.php
+++ templates/Ceremonies/add.php
@@ -1,12 +1,20 @@
 <h1 class="page-title">Add Ceremony</h1>
-<?php
-echo $this->Form->create($ceremony);
+<?= $this->Form->create($ceremony);?>
 //$this->Form->templates(
 //    ['dateWidget' => '{{day}}{{month}}{{year}}']
 //);
 
+<div class="form-group">
+    <?= $this->Form->control('registration_year', ['label'=> 'Registration Year', 'class' => 'form-control','disabled' => true]); ?>
+</div>
+<div class="form-group">
+    <?= echo $this->Form->control('night'); ?>
+</div>
+
+
+<?php
 echo $this->Form->control('registration_year', ['disabled' => true]);
-echo $this->Form->control('night');
+
 echo $this->Form->control('ceremony_date', ['type' => 'date', 'value' => date('Y-m-d')]);
 echo $this->Form->control('ceremony_time', ['type' => 'time']);
 //echo $this->Form->dateTime('test', ['dataWidget' => '{{day}}{{month}}{{year}}']);
