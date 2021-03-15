0e4b142b34f003347c66d0789858274ab630c21f
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2021-03-15 10:27:10 -0700

    Fixed nesting issues with bracelet display on registration form.

--- templates/element/registrationForm/stepTwo.php
+++ templates/element/registrationForm/stepTwo.php
@@ -97,14 +97,19 @@
 
 <!-- 35 YR BRACELET CONTROLS -->
 <div class="row" v-if="displayBracelet">
-    <div class="form-group col-3">
-        <label for="bracelet_size">Size</label>
-        <select  class="form-control" name="bracelet_size" id="bracelet_size" v-model="braceletSize">
-            <option disabled selected>Choose Size</option>
-            <option>Fits 6 ½″ - 7 ½″ circumference wrists</option>
-            <option>Fits 7 ½″ - 8 ½″ circumference wrists</option>
-        </select>
+
+    <div class="col-4"></div>
+    <div class="col-4">
+        <div class="form-group">
+            <label for="bracelet_size">Size</label>
+            <select  class="form-control" name="bracelet_size" id="bracelet_size" v-model="braceletSize">
+                <option disabled selected>Choose Size</option>
+                <option>Fits 6 ½″ - 7 ½″ circumference wrists</option>
+                <option>Fits 7 ½″ - 8 ½″ circumference wrists</option>
+            </select>
+        </div>
     </div>
+    <div class="col-4"></div>
 </div>
 
 
